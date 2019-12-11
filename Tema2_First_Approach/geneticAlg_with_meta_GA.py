4c3a2d63ffac442d793fa692395e7f217856aada
Georgiana Ingrid CITEA (STOLERU) <istoleru@ias.bitdefender.biz>
2019-12-11 23:31:30 +0200

    Added GA for determining hyperparameters

--- Tema2_First_Approach/geneticAlg_with_meta_GA.py
+++ Tema2_First_Approach/geneticAlg_with_meta_GA.py
@@ -8,20 +8,14 @@
 #Mutation occures with a certain probability. (bit flip)
 #Replacement
 
-import random
-import numpy
-import ga
-
 """ Aux data for algorithm """
-num_generations = 100
-best_outputs = []
-number_chromosomes = 20
-num_parents_mating = 4
+best_outputs, number_chromosomes, num_parents_mating = [], 20, 4
 
 """ Input data """
-number_bidders = 4;
-number_objects = 5;
-number_bids = 12;
+number_bidders, number_objects, number_bids = 4, 5, 12
+
+""" Information for meta algorithm """
+number_indiv_meta, number_bits_meta, num_generations_meta, prob_mutation_meta, prob_cross_meta = 20, 20, 20, 0.3, 0.2
 
 offers = [[1, [1, 2, 3, 4, 5], 173],
           [1, [1, 2, 4, 5], 173],
@@ -43,9 +37,105 @@ def intersection(lst1, lst2):
     return list(set(lst1) & set(lst2)) 
 
 
+# The meta algorithm will have 20 individuals, each of them represented on 20 bits (first 6 for mutation probability, next 6 for 
+# crossover probability, last 8 for number of generations )
+def individual_generation_meta():
+	new_meta_pop = []
+	for index_i in xrange(number_indiv_meta):
+		current_individual = numpy.random.randint(2, size=number_bits_meta)	
+		new_meta_pop.append(current_individual)
+
+	return new_meta_pop
+
+def select_parents_meta(fitness, num_parents):
+	parents = []
+	for i in xrange(num_parents):
+		parents.append(fitness.pop()[0])			
+	
+	return parents
+
+def gawd_meta():
+	new_population = individual_generation_meta()
+	new_population = numpy.asarray(new_population)
+	for generation in xrange(num_generations_meta):
+		all_fitnesses_indiv = []		
+		for individual in new_population:
+			prob_mutation, prob_crossover, generationss = 0, 0, 0
+			prob_mutation_bits = individual[:6]
+			prob_crossover_bits = individual[6:12]
+			num_gen = individual[12:18]
+
+			for i in range(6):
+				prob_mutation += prob_mutation_bits[i] * pow(2, i)
+				prob_crossover += prob_crossover_bits[i] * pow(2, i)
+				generationss += num_gen[i] * pow(2, i)
+
+			prob_mutation = prob_mutation/ 100
+			prob_crossover = prob_crossover/ 100 	
+
+			fitness = gawd(prob_mutation, prob_crossover, generationss)
+			all_fitnesses_indiv.append([individual, fitness])
+		
+		""" Sort all after best fitness to choose parents """
+		all_fitnesses_indiv.sort(key=lambda all_fitnesses_indiv: all_fitnesses_indiv[1])
+		""" Select parents by elitism """
+		parents = select_parents_meta(all_fitnesses_indiv, 4)
+		
+		crossover_individuals = []
+		new = []
+		for i in xrange(len(new_population) - len(parents)):
+			new = []
+			parent1 = random.choice([0,1,2,3]) 					
+			parent2 = random.choice([0,1,2,3]) 					
+			crossover_point = random.randint(0, len(parents)) 
+
+			for i in range(crossover_point):
+				new.append(parents[parent1][i])
+			for i in range(crossover_point, len(parents[parent1])):
+				new.append(parents[parent2][i])
+
+			crossover_individuals.append(new)	
+		
+		for el in parents:
+			crossover_individuals.append(el)
+
+		for el in crossover_individuals:
+			odds = random.uniform(0, 1)		
+			if odds > prob_mutation_meta:
+				k = random.randint(0, len(el)-1)
+				if el[k] == 1:
+					el[k] = 0
+				else:
+					el[k] = 1
+			
+	for el in crossover_individuals:
+			prob_mutation, prob_crossover, generationss = 0, 0, 0
+			prob_mutation_bits = individual[:6]
+			prob_crossover_bits = individual[6:12]
+			num_gen = individual[12:18]
+
+			for i in range(6):
+				prob_mutation += prob_mutation_bits[i] * pow(2, i)
+				prob_crossover += prob_crossover_bits[i] * pow(2, i)
+				generationss += num_gen[i] * pow(2, i)
+
+			prob_mutation = prob_mutation/ 100
+			prob_crossover = prob_crossover/ 100 	
+				
+			fitness = gawd(prob_mutation, prob_crossover, generationss)
+			all_fitnesses_indiv.append([(prob_mutation, prob_crossover, generationss), fitness])
+		
+	all_fitnesses_indiv.sort(key=lambda all_fitnesses_indiv: all_fitnesses_indiv[1])
+
+	print("Best elements: fitness {0}, prob_mutation: {1}, prob_crossover: {2}, number_generations: {3}".format(all_fitnesses_indiv[-1][1],
+	all_fitnesses_indiv[-1][0][0], all_fitnesses_indiv[-1][0][1], all_fitnesses_indiv[-1][0][2]))
+
+import numpy
+from past.builtins import xrange
+
 def individual_generation():		
 	new_population = []
-	for index_i in range(number_chromosomes):
+	for index_i in xrange(number_chromosomes):
 		current_individual = numpy.random.randint(2, size=number_bids)		
 		all_ones = [i for i, e in enumerate(current_individual) if e == 1]
 		for el in all_ones:
@@ -61,14 +151,13 @@ def individual_generation():
 	return new_population	
 
 
-
 def select_mating_pool(pop, fitness, num_parents):	
 	pop = numpy.asarray(pop)
 	fitness = numpy.asarray(fitness)
     # Selecting the best individuals in the current generation as parents for producing the offspring of the next generation.
 	parents = numpy.empty((num_parents, pop.shape[1]))
 	
-	for parent_num in range(num_parents):
+	for parent_num in xrange(num_parents):
 		max_fitness_idx = numpy.where(fitness == numpy.max(fitness))
 		max_fitness_idx = max_fitness_idx[0][0]
 		parents[parent_num, :] = pop[max_fitness_idx, :]
@@ -81,7 +170,7 @@ def cal_pop_fitness(pop):
 	all_fitness = []
 	for el in pop:
 		fitness = 0
-		for index_i in range(len(el)):
+		for index_i in xrange(len(el)):
 			fitness += el[index_i]*offers[index_i][2]
 		
 		all_fitness.append(fitness)
@@ -94,7 +183,7 @@ def crossover(parents, offspring_size):
 	# The point at which crossover takes place between two parents. Usually, it is at the center.
 	crossover_point = numpy.uint8(offspring_size[1]/2)
 
-	for k in range(offspring_size[0]):
+	for k in xrange(offspring_size[0]):
 		# Index of the first parent to mate.
 		parent1_idx = k%parents.shape[0]
 		# Index of the second parent to mate.
@@ -118,16 +207,18 @@ def crossover(parents, offspring_size):
 	return offspring
      
 
-def mutation(offspring_crossover):	
+import random
+
+def mutation(offspring_crossover, prob_mutation):	
 	# Mutation changes a single gene in each offspring randomly.
 	# For each individual in the current population
-	for idx in range(offspring_crossover.shape[0]):
+	for idx in xrange(offspring_crossover.shape[0]):
 		current_individual = offspring_crossover[idx]		
 		all_ones = [i for i, e in enumerate(current_individual) if e == 1]		
 		odds = random.uniform(0, 1)		
 		chances = 0
 		ok = 0
-		if odds >= 0.5:
+		if odds >= prob_mutation:
 			while ok == 0 and chances < 10:
 				ok = 1
 				chances += 1
@@ -144,19 +235,25 @@ def mutation(offspring_crossover):
 
 	return offspring_crossover
 
-def gawd():
+def gawd(prob_mutation, prob_cross, num_generations):
 	new_population = individual_generation()
 	new_population = numpy.asarray(new_population)
-	for generation in range(num_generations):		
+	for generation in xrange(num_generations):		
 		fitness = cal_pop_fitness(new_population)
 		parents = select_mating_pool(new_population, fitness, num_parents_mating)
-		offspring_crossover = crossover(parents, offspring_size=(number_chromosomes - parents.shape[0], number_bids))
-		# Adding some variations to the offsrping using mutation.
-		offspring_mutation = mutation(offspring_crossover)
 
-		new_population[0:parents.shape[0], :] = parents
-		new_population[parents.shape[0]:, :] = offspring_mutation
-		new_population = numpy.asarray(new_population)
+		odds = random.uniform(0, 1)				
+		if odds > prob_cross:
+			offspring_crossover = crossover(parents, offspring_size=(number_chromosomes - parents.shape[0], number_bids))
+			# Adding some variations to the offsrping using mutation.
+			offspring_mutation = mutation(offspring_crossover, prob_mutation)
+			new_population[0:parents.shape[0], :] = parents
+			new_population[parents.shape[0]:, :] = offspring_mutation
+			new_population = numpy.asarray(new_population)
+
+		else:
+			offspring_mutation = mutation(new_population, prob_mutation)
+			new_population = offspring_mutation
 
 		# The best result in the current iteration.
 		# print("Best result at iteration {0}: {1}".format(generation, numpy.max(fitness)))
@@ -168,8 +265,15 @@ def gawd():
 	best_match_idx = numpy.where(fitness == numpy.max(fitness))
 
 
-	print("Best solution : ", new_population[best_match_idx[0][0], :])
-	print("Best solution fitness : ", fitness[best_match_idx[0][0]])
+	#print("Best solution : ", new_population[best_match_idx[0][0], :])
+	#print("Best solution fitness : ", fitness[best_match_idx[0][0]])
+
+	return fitness[best_match_idx[0][0]]
 	
+import time	
 if __name__ == '__main__':
-	gawd()
+    start_time = time.time()
+   	#gawd(0.5, 0.5)
+    gawd_meta()
+    print("--- %s seconds ---" % (time.time() - start_time))
+	
